25075095f34541597dcc724884b64466e534259b
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2024-01-18 16:35:29 -0300

    removendo atributo desnecessário

--- src/main/java/rodrigues/igor/csv/CSVReader.java
+++ src/main/java/rodrigues/igor/csv/CSVReader.java
@@ -2,7 +2,6 @@ package rodrigues.igor.csv;
 
 import com.opencsv.exceptions.CsvValidationException;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,10 +11,10 @@ public class CSVReader {
     private static final String csvFile = "src/main/java/rodrigues/igor/database/data/nomes.csv";
 
     public ArrayList<String> getNames() throws FileNotFoundException {
-        return get("nome", csvFile);
+        return get(csvFile);
     }
 
-    public ArrayList<String> get(String name, String path) throws FileNotFoundException {
+    public ArrayList<String> get(String path) {
         try(com.opencsv.CSVReader reader = new com.opencsv.CSVReader(new FileReader(path))) {
             String[] nextLine;
             ArrayList<String> result = new ArrayList<>();
